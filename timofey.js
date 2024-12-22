e48f59b70366b3d9a00d011d7971d6300a67749a
alek981 <alek.efremov@icloud.com>
2024-12-22 15:18:57 +0300

    ABDATEtMOFEY

--- timofey.js
+++ timofey.js
@@ -1,10 +1,9 @@
-    let currentSec = 0;
-    
-    function secCounter(){
-        setInterval(() => {
-            currentSec++
-            clockDisplay.innerHTML = currentSec;
-        }, 1000);
-    }
-
-    secCounter();
+let currentSec = 0;
+const secCounter = document.querySelector(".secCounter");
+function secCounter() {
+  setInterval(() => {
+    currentSec++;
+    clockDisplay.innerHTML = currentSec;
+  }, 1000);
+}
+secCounter();
